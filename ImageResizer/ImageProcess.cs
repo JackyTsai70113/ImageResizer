f1b39ed065b231e9816ac81b3aac102d9ea42496
JackyTsai70113 <j70113jacky@yahoo.com.tw>
2020-03-27 03:36:59 +0800

    [32~36%] 將品質優化的code還原順序, 並使用平行處理的ForEach

--- ImageResizer/ImageProcess.cs
+++ ImageResizer/ImageProcess.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Drawing;
@@ -13,108 +14,17 @@ using System.Threading.Tasks;
 namespace ImageResizer {
 
     public class ImageProcess {
-        private static object lockObj = new object();
-        private static List<int> tsl2 = new List<int>();
-        private static List<int> tsl3 = new List<int>();
-        private static List<int> tsl4 = new List<int>();
-        private static List<int> tsl5 = new List<int>();
-        private static List<int> tsl6 = new List<int>();
-
-        private Bitmap ProcessImage(string filePath, double scale) {
-
-            #region 6~10ms
-
-            Image imgPhoto = Image.FromFile(filePath);
-
-            #endregion 6~10ms
-
-            #region 可忽略
-
-            int sourceWidth = imgPhoto.Width;
-            int sourceHeight = imgPhoto.Height;
-
-            int destionatonWidth = (int)(sourceWidth * scale);
-            int destionatonHeight = (int)(sourceHeight * scale);
-
-            #endregion 可忽略
-
-            #region 30~80ms
-
-            return processBitmap((Bitmap)imgPhoto,
-                 sourceWidth, sourceHeight,
-                 destionatonWidth, destionatonHeight);
-
-            #endregion 30~80ms
-        }
-
-        private Task<Bitmap> ProcessImageAsync(string filePath, double scale) {
-            //Write($"[ProcessImageAsync] 設定圖片縮放格式參數 [{filePath}]...");
-
-            #region 6~10ms
-
-            Image imgPhoto = Image.FromFile(filePath);
-
-            #endregion 6~10ms
-
-            #region 可忽略
-
-            int sourceWidth = imgPhoto.Width;
-            int sourceHeight = imgPhoto.Height;
-
-            int destionatonWidth = (int)(sourceWidth * scale);
-            int destionatonHeight = (int)(sourceHeight * scale);
-
-            #endregion 可忽略
-
-            #region 30~80ms
-
-            //Write($"[ProcessImageAsync] 設定完畢 [{filePath}]");
-            //var a = processBitmap((Bitmap)imgPhoto,
-            //    sourceWidth, sourceHeight,
-            //    destionatonWidth, destionatonHeight);
-            return Task.Run(() => NewProcessBitmap((Bitmap)imgPhoto,
-                sourceWidth, sourceHeight,
-                destionatonWidth, destionatonHeight));
-
-            #endregion 30~80ms
-        }
 
         /// <summary>
-        /// [非同步] 針對指定圖片進行縮放作業
+        /// 紀錄當下執行序ID及時間
         /// </summary>
-        /// <param name="img">圖片來源</param>
-        /// <param name="srcWidth">原始寬度</param>
-        /// <param name="srcHeight">原始高度</param>
-        /// <param name="newWidth">新圖片的寬度</param>
-        /// <param name="newHeight">新圖片的高度</param>
-        /// <returns></returns>
-        private Bitmap NewProcessBitmap(Bitmap img, int srcWidth, int srcHeight, int newWidth, int newHeight) {
-            Bitmap resizedbitmap = new Bitmap(newWidth, newHeight);
-            Graphics g = Graphics.FromImage(resizedbitmap);
-            g.Clear(Color.Transparent); //久
-            g.DrawImage(img,
-                new Rectangle(0, 0, newWidth, newHeight),
-                new Rectangle(0, 0, srcWidth, srcHeight),
-                GraphicsUnit.Pixel); //久
-            g.InterpolationMode = InterpolationMode.High;
-            g.SmoothingMode = SmoothingMode.HighQuality;
-
-            //clearTask.Wait();
-            return resizedbitmap;
-            //Write(" 1 Task<Bitmap>" + img.ToString());
-            //Bitmap resizedbitmap = new Bitmap(newWidth, newHeight);
-            //Graphics g = Graphics.FromImage(resizedbitmap);
-            //Write(" 2 Task<Bitmap>" + img.ToString());
-            //g.InterpolationMode = InterpolationMode.High;
-            //g.SmoothingMode = SmoothingMode.HighQuality;
-            //g.Clear(Color.Transparent);
-            //Write(" 3 Task<Bitmap>" + img.ToString());
-            //g.DrawImage(img,
-            //    new Rectangle(0, 0, newWidth, newHeight),
-            //    new Rectangle(0, 0, srcWidth, srcHeight),
-            //    GraphicsUnit.Pixel);
-            //Write(" 4 Task<Bitmap>" + img.ToString());
-            //return new Task<Bitmap>(resizedbitmap);
+        /// <param name="content"></param>
+        public static void Write(string content = "") {
+            int threadId = Thread.CurrentThread.ManagedThreadId;
+            Console.WriteLine(
+                $"#{threadId} " +
+                $"[{DateTime.Now:HH:mm:ss.fff}] " +
+                $"{content} ");
         }
 
         /// <summary>
@@ -126,50 +36,17 @@ namespace ImageResizer {
         /// <param name="newWidth">新圖片的寬度</param>
         /// <param name="newHeight">新圖片的高度</param>
         /// <returns></returns>
-        private Bitmap processBitmap(Bitmap img, int srcWidth, int srcHeight, int newWidth, int newHeight) {
-            Write($"1 " + img.GetHashCode());
-            DateTime t1 = DateTime.Now;
+        private Bitmap ProcessBitmap(Bitmap img, int srcWidth, int srcHeight, int newWidth, int newHeight) {
             Bitmap resizedbitmap = new Bitmap(newWidth, newHeight);
             Graphics g = Graphics.FromImage(resizedbitmap);
-            DateTime t2 = DateTime.Now;
-            int ts2 = (DateTime.Now - t1).Milliseconds;
-            Write($"2 " + img.GetHashCode());
             g.InterpolationMode = InterpolationMode.High;
-            DateTime t3 = DateTime.Now;
-            int ts3 = (DateTime.Now - t2).Milliseconds;
-            Write($"3 " + img.GetHashCode());
             g.SmoothingMode = SmoothingMode.HighQuality;
-            DateTime t4 = DateTime.Now;
-            int ts4 = (DateTime.Now - t3).Milliseconds;
-            Write($"4 " + img.GetHashCode());
             g.Clear(Color.Transparent);
-            DateTime t5 = DateTime.Now;
-            int ts5 = (DateTime.Now - t4).Milliseconds;
-            Write($"5 " + img.GetHashCode());
             g.DrawImage(img,
                 new Rectangle(0, 0, newWidth, newHeight),
                 new Rectangle(0, 0, srcWidth, srcHeight),
                 GraphicsUnit.Pixel);
-            DateTime t6 = DateTime.Now;
-            int ts6 = (DateTime.Now - t5).Milliseconds;
-            Write($"6 " + img.GetHashCode());
-            tsl2.Add(ts2);
-            tsl3.Add(ts3);
-            tsl4.Add(ts4);
-            tsl5.Add(ts5);
-            tsl6.Add(ts6);
             return resizedbitmap;
-        }
-
-        /// <summary>
-        /// 紀錄當下執行序ID及時間
-        /// </summary>
-        /// <param name="content"></param>
-        public static void Write(string content = "") {
-            Console.WriteLine(
-                $"#{Thread.CurrentThread.ManagedThreadId} " +
-                $"[{DateTime.Now:HH:mm:ss.fff}] " +
-                $"{content} ");
         }
 
         /// <summary>
@@ -196,7 +73,6 @@ namespace ImageResizer {
         public void ResizeImages(string sourcePath, string destPath, double scale) {
             var allFiles = FindImages(sourcePath);
             foreach (var filePath in allFiles) {
-                //Write($"1 [{filePath}]");
 
                 #region 9ms
 
@@ -204,8 +80,6 @@ namespace ImageResizer {
                 string imgName = Path.GetFileNameWithoutExtension(filePath);
 
                 #endregion 9ms
-
-                //Write($"2 [{filePath}]");
 
                 #region 0ms
 
@@ -217,17 +91,13 @@ namespace ImageResizer {
 
                 #endregion 0ms
 
-                //Write($"3 [{filePath}]");
-
                 #region 75~126ms
 
-                Bitmap processedImage = processBitmap((Bitmap)imgPhoto,
+                Bitmap processedImage = ProcessBitmap((Bitmap)imgPhoto,
                     sourceWidth, sourceHeight,
                     destionatonWidth, destionatonHeight);
 
                 #endregion 75~126ms
-
-                //Write($"4 [{filePath}]");
 
                 #region 44ms
 
@@ -235,66 +105,41 @@ namespace ImageResizer {
                 processedImage.Save(destFile, ImageFormat.Jpeg);
 
                 #endregion 44ms
-
-                //Write($"5 [{filePath}]");
             }
         }
 
         /// <summary>
-        /// [非同步] 進行圖片的縮放作業
+        /// [平行計算] 進行圖片的縮放作業
         /// </summary>
         /// <param name="sourcePath">圖片來源目錄路徑</param>
         /// <param name="destPath">產生圖片目的目錄路徑</param>
         /// <param name="scale">縮放比例</param>
-        public Task ResizeImagesAsync(string sourcePath, string destPath, double scale) {
-            Write($"尋找 [{sourcePath}] 中的所有圖片...");
+        public void ParallelResizeImages(string sourcePath, string destPath, double scale) {
+            //Write($"尋找 [{sourcePath}] 中的所有圖片...");
             var allFiles = FindImages(sourcePath);
-            Write($"尋找完畢.");
-            List<Task> tasks = new List<Task>();
-            Write($"開始逐一處理圖片...");
-            foreach (var filePath in allFiles) {
-                //Write($"開始處理圖片 [{filePath}]");
-                Task<Bitmap> ProcessImageAsyncTask = ProcessImageAsync(filePath, scale);
+            //Write($"尋找完畢.");
+            //Write($"開始逐一處理圖片...");
+            Parallel.ForEach(allFiles, (filePath) => {
+                Image imgPhoto = Image.FromFile(filePath);
+
+                int sourceWidth = imgPhoto.Width;
+                int sourceHeight = imgPhoto.Height;
+
+                int destionatonWidth = (int)(sourceWidth * scale);
+                int destionatonHeight = (int)(sourceHeight * scale);
+
+                #region 30~80ms
+
+                Bitmap bitmap = ProcessBitmap((Bitmap)imgPhoto,
+                    sourceWidth, sourceHeight,
+                    destionatonWidth, destionatonHeight);
+
+                #endregion 30~80ms
 
                 string imgName = Path.GetFileNameWithoutExtension(filePath);
                 string destFile = Path.Combine(destPath, imgName + ".jpg");
-
-                //Task SaveImageTask = ProcessImageAsyncTask.ContinueWith((antecedent) =>
-                tasks.Add(ProcessImageAsyncTask.ContinueWith(t => {
-                    t.Result.Save(destFile, ImageFormat.Jpeg);
-                    Write($"已儲存 [{filePath}]");
-                }));
-            }
-            Task.WhenAll(tasks).Wait();
-            Write("After Task.WhenAll(tasks).Wait();");
-            /*
-            Write(tsl2.Average().ToString());
-            Write(tsl2.Max().ToString());
-            Write(tsl2.Min().ToString());
-            Console.WriteLine();
-            Write(tsl3.Average().ToString());
-            Write(tsl3.Max().ToString());
-            Write(tsl3.Min().ToString());
-            Console.WriteLine();
-            Write(tsl4.Average().ToString());
-            Write(tsl4.Max().ToString());
-            Write(tsl4.Min().ToString());
-            Console.WriteLine();
-            Write(tsl5.Average().ToString());
-            Write(tsl5.Max().ToString());
-            Write(tsl5.Min().ToString());
-            Console.WriteLine();
-            Write(tsl6.Average().ToString());
-            Write(tsl6.Max().ToString());
-            Write(tsl6.Min().ToString());
-            Console.WriteLine();
-            */
-            Task.WhenAll(tasks).Wait();
-            return Task.CompletedTask;
-        }
-
-        public Task TaskSaveProcessedImageAsync(Bitmap processedImage, string destFile) {
-            return Task.Run(() => processedImage.Save(destFile, ImageFormat.Jpeg));
+                bitmap.Save(destFile, ImageFormat.Jpeg);
+            });
         }
 
         /// <summary>
--- ImageResizer/Program.cs
+++ ImageResizer/Program.cs
@@ -1,33 +1,62 @@
 ﻿using System;
 using System.Diagnostics;
-using System.Drawing;
 using System.IO;
-using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Runtime.Remoting.Contexts;
-using System.Text;
-using System.Threading;
 using System.Threading.Tasks;
 
 namespace ImageResizer {
 
     internal class Program {
-        private static readonly Stopwatch newSw = new Stopwatch();
 
-        private static readonly Stopwatch origSw = new Stopwatch();
+        /// <summary>
+        /// 新版程式時間
+        /// </summary>
+        private static long newSpan = 0;
 
-        private static long newSpan;
-
-        private static long origSpan;
+        /// <summary>
+        /// 舊版程式時間
+        /// </summary>
+        private static long origSpan = 1;
 
         private static void Main(string[] args) {
-            Task.Run(() => NewHandleImage()).Wait();
-            Task.Run(() => OrigHandleImage()).Wait();
+            ImageProcess _imageProcess = new ImageProcess();
+
+            // 設定來源資料夾及目標資料夾
+            string sourcePath = Path.Combine(Environment.CurrentDirectory, "images");
+            string destinationPath = Path.Combine(Environment.CurrentDirectory, "output");
+
+            // 清除目標資料夾
+            _imageProcess.Clean(destinationPath);
+
+            ImageProcess.Write("default(origSpan): " + GetExecutionTime(Task.Run(() => {
+                _imageProcess.ParallelResizeImages(sourcePath, destinationPath, 2.0);
+            })));
+            //imageProcess.ParallelResizeImages(sourcePath, destinationPath, 2.0);
+
+            // 新版程式
+            ImageProcess.Write($"平行處理縮放圖片...");
+            newSpan = GetExecutionTime(Task.Run(() => {
+                _imageProcess.ParallelResizeImages(sourcePath, destinationPath, 2.0);
+            }));
+
+            ImageProcess.Write("平行處理圖片完畢.");
+
+            // 清除目標資料夾
+            _imageProcess.Clean(destinationPath);
+
+            // 原版程式
+            ImageProcess.Write($"原版縮放圖片程式...");
+            origSpan = GetExecutionTime(Task.Run(() => {
+                _imageProcess.ResizeImages(sourcePath, destinationPath, 2.0);
+            }));
+            ImageProcess.Write("處理圖片完畢.");
+
+            // 計算並印出此次結果
             Console.WriteLine($"New: {newSpan} ms");
             Console.WriteLine($"Orig: {origSpan} ms");
-            Console.WriteLine(string.Format("效能提升比例：{0:0.000}", ((double)origSpan - newSpan) / origSpan));
+            Console.WriteLine(string.Format("效能提升比例: {0:00.0}%", ((double)origSpan - newSpan) / origSpan * 100));
 
-            Console.ReadLine();
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
 
             // 1. 程式碼需兼顧可讀性(請適度排版)與執行效率
             // 2. 請紀錄修改前與修改後的效能改善幅度(百分比)
@@ -35,42 +64,17 @@ namespace ImageResizer {
             // 4. 效能提升比例公式：(Orig - New) / Orig
         }
 
-        private static void OrigHandleImage() {
-            string sourcePath = Path.Combine(Environment.CurrentDirectory, "images");
-            string destinationPath = Path.Combine(Environment.CurrentDirectory, "output");
-
-            ImageProcess imageProcess = new ImageProcess();
-            //imageProcess.Clean(destinationPath);
-
-            origSw.Start();
-            //task.Wait();
-            imageProcess.ResizeImages(sourcePath, destinationPath, 2.0);
-            origSw.Stop();
-            origSpan = origSw.ElapsedMilliseconds;
-            ImageProcess.Write("[HandleImage] Process done.");
-        }
-
-        private static void NewHandleImage() {
-            Console.WriteLine(
-                $"#{Thread.CurrentThread.ManagedThreadId} " +
-                $"[{DateTime.Now:HH:mm:ss.fff}] ");
-            string sourcePath = Path.Combine(Environment.CurrentDirectory, "images");
-            string destinationPath = Path.Combine(Environment.CurrentDirectory, "output");
-
-            ImageProcess imageProcess = new ImageProcess();
-            ImageProcess.Write($"清除目標資料夾中資料...");
-            imageProcess.Clean(destinationPath);
-            ImageProcess.Write($"清除完畢");
-            Console.WriteLine(
-                $"#{Thread.CurrentThread.ManagedThreadId} " +
-                $"[{DateTime.Now:HH:mm:ss.fff}] ");
-            newSw.Start();
-            imageProcess.ResizeImagesAsync(sourcePath, destinationPath, 2.0).Wait();
-            //imageProcess.ResizeImages(sourcePath, destinationPath, 2.0);
-            //task.Wait();
-            newSw.Stop();
-            newSpan = newSw.ElapsedMilliseconds;
-            ImageProcess.Write("圖片處理完畢");
+        /// <summary>
+        /// 取得Task執行時間
+        /// </summary>
+        /// <param name="task"></param>
+        /// <returns>time(ms)</returns>
+        private static long GetExecutionTime(Task task) {
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            task.Wait();
+            sw.Stop();
+            return sw.ElapsedMilliseconds;
         }
     }
 }
